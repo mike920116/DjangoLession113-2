1cb359a4f595e0d8bb8f0a3dd0cf76b4d220a88e
mike920116 <10856037@ntub.edu.tw>
2025-02-24 11:47:59 +0800

    Django 管理後台（Admin Site）註冊 Post 模型，並自訂其顯示方式

--- 20250224myproject/mblog/mysite/admin.py
+++ 20250224myproject/mblog/mysite/admin.py
@@ -1,3 +1,11 @@
+#  Django 管理後台（Admin Site）註冊 Post 模型，並自訂其顯示方式
 from django.contrib import admin
+from .models import Post  # 匯入 Post 模型
 
-# Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'pub_date')  # 列表顯示的欄位
+    search_fields = ('title',)  # 可在管理後台搜尋 title
+    list_filter = ('pub_date',)  # 依照日期篩選
+    prepopulated_fields = {'slug': ('title',)}  # 當輸入 title 時，自動生成 slug
+
+admin.site.register(Post, PostAdmin)

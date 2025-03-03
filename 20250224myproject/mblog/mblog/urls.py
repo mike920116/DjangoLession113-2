fa1f632a5e6c1d26d861ce09c06eb544fac8b289
mike920116 <10856037@ntub.edu.tw>
2025-03-03 13:16:15 +0800

    新增三個路徑

--- 20250224myproject/mblog/mblog/urls.py
+++ 20250224myproject/mblog/mblog/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from django.contrib import admin
-from mysite.views import homepage, showpost
+from mysite.views import homepage, showpost, post_list, about
 
 urlpatterns = [
-    path('', homepage),
+    path('', homepage, name='home'),
     path('post/<slug:slug>/', showpost),
+    path('post-list/', post_list, name='post_list'),  # 為貼文列表頁面定義名稱
+    path('about/', about, name='about'),  # 為關於我們頁面定義名稱
     path('admin/', admin.site.urls),
 ]
